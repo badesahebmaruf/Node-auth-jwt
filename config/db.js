0eecb2de09b0c3ba3950fa9c41c50cc4fffb3d5e
badesahebmaruf <bademaruf123@gmail.com>
2023-03-26 15:34:47 +0530

    added db url in env

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 module.exports ={
     'secret' : "10xauthsecret",
     // 'db':"mongodb://localhost:27017/node-auth"
-    'db':"mongodb+srv://bademarufm:10xacademy@cluster0.rbgealw.mongodb.net/?retryWrites=true&w=majority"
+    // 'db':"mongodb+srv://bademarufm:10xacademy@cluster0.rbgealw.mongodb.net/?retryWrites=true&w=majority"
+    "db" :process.env.Db_url
 }
