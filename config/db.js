3a80b121c0a8e93602d6f5632a339b8537ae6f9b
badesahebmaruf <bademaruf123@gmail.com>
2023-03-26 15:19:16 +0530

    Added Node api with jwt auth

---
+++ config/db.js
@@ -0,0 +1,5 @@
+module.exports ={
+    'secret' : "10xauthsecret",
+    // 'db':"mongodb://localhost:27017/node-auth"
+    'db':"mongodb+srv://bademarufm:10xacademy@cluster0.rbgealw.mongodb.net/?retryWrites=true&w=majority"
+}
